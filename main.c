a681fc6d7d08601fc224cde5f7cdae529d7411fe
TheAwesomePanda <l.maslowski2012@gmail.com>
2017-04-02 20:36:32 +0200

    Update main.c

--- main.c
+++ main.c
@@ -36,7 +36,7 @@ int main(int argc, char *argv[]) {
     register_callback(sortingAlgos, "selectionSort", SelectionSort);
     register_callback(sortingAlgos, "InsertionSort", InsertionSort);
     register_callback(sortingAlgos, "HeapSort", HeapSort);
-    register_callback(sortingAlgos, "QuickSort", QucikSort);
+    register_callback(sortingAlgos, "QuickSort", QuickSort);
 
 
     benchmark_algos(sortingAlgos, testInstances);
