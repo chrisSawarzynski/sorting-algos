8c6ce7bf3c383ea1f27ff0b9bf730805f9ecf192
TheAwesomePanda <l.maslowski2012@gmail.com>
2017-04-10 10:09:28 +0200

    Add files via upload

---
+++ qsi.h
@@ -0,0 +1,61 @@
+#ifndef QSI_H_INCLUDED
+#define QSI_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Stos
+{
+    int l;
+    int p;
+};
+
+void QuickSortIterative(int tab[],int n,struct Stos stos[])
+{
+    int i, j, l, p;
+    int pivot, temp;
+    int aktStos;
+
+    aktStos=0;
+    stos[0].l = 0;
+    stos[0].p = n-1;
+    do
+    {
+        l = stos[aktStos].l;
+        p = stos[aktStos].p;
+        aktStos--;
+        do
+        {
+            i = l;
+            j = p;
+            pivot = tab[(l+p)/2];
+            do
+            {
+                while(tab[i]<pivot)
+                    i=i+1;
+                while(pivot<tab[j])
+                    j=j-1;
+                if (i<=j)
+                {
+                    temp= tab[i];
+                    tab[i] = tab[j];
+                    tab[j] = temp;
+                    i++;
+                    j--;
+                }
+            }
+            while(i<=j);
+            if (i<p)
+            {
+                aktStos++;
+                stos[aktStos].l = i;
+                stos[aktStos].p = p;
+            }
+            p = j;
+        }
+        while(l<p);
+    }
+    while(aktStos>=0);
+}
+
+#endif // QSI_H_INCLUDED
